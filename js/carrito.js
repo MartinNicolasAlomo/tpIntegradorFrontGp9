6509bfe7de1b0053a124e58342cce7aca6b02975
Nahuel Vega <nahueljv@gmail.com>
2025-07-04 01:30:54 -0300

    -Adaptamos diseño al carrito y al index -Modificamos las variables que consume de localStorages -Agregamos comentarios varios en los JS -Agregamos los estilos necesarios para implementar el carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,6 +1,12 @@
+//INICIALIZO EL CARRITO
+let carrito= JSON.parse(localStorage.getItem("carrito")) || []; 
+
+// =================    MOSTRAR CARRITO    ======================
+// -mostrarCarrito() crea dinamicamente todas las tarjetas de cada producto dentro del carrito 
+// -Agrega las funcionalizades de boton
+// -Actualizo el carrito dependiendo de la accion que se ejecute(restar/sumar/eliminar)
 const contenedorCarrito = document.getElementById("lista-producto");
 function mostrarCarrito(){
-    let carrito= JSON.parse(localStorage.getItem("carrito")) || []; 
     let totalCarrito=0;
     let cantidadCarrito=0;
     contenedorCarrito.innerHTML="";
@@ -10,70 +16,82 @@ function mostrarCarrito(){
         const div = document.createElement("div");
         div.classList.add("producto-carrito");
         div.innerHTML=` 
-            <img src="${producto.imagen}" alt="${producto.nombre}">
-            <div class="info">
+            <img src="${producto.img}" alt="${producto.nombre}">
+            <div class="info-producto">
                 <p><strong>${producto.nombre}</strong></p>
                 <p>Precio unitario: $${producto.precio}</p>
                 <p>Cantidad: <span id="cant">${producto.cantidad}</span></p>
                 <p>Subtotal: $<span id="sub">${subTotal}</span></p>
             </div>
-            <div class="controles">
-                <button class="btn-restar"onclick="restarProducto('${producto.id}')">−</button>
-                <button class="btn-agregar" onclick="sumarProducto('${producto.id}')">+</button>
-                <button class="btn-eliminar">Eliminar</button>
+            <div class="controles-accion">
+                <button class="boton-restar"onclick="restarProducto('${producto.id}')">−</button>
+                <button class="boton-agregar-carrito" onclick="sumarProducto('${producto.id}')">+</button>
+                <button class="boton-eliminar">Eliminar</button>
             </div>
         `;
         contenedorCarrito.appendChild(div);
         //Agrego el evento al boton eliminar
-        div.querySelector('.btn-eliminar').addEventListener('click',()=>EliminarProducto(producto.id));
+        div.querySelector('.boton-eliminar').addEventListener('click',()=>EliminarProducto(producto.id));
         totalCarrito=totalCarrito+subTotal;
         cantidadCarrito=cantidadCarrito+producto.cantidad;
         //Voy calculando el total de los productos
     }
-    const buttonFinalizar = document.getElementById("btn-finalizar");
+    const buttonFinalizar = document.getElementById("boton-finalizar");
     buttonFinalizar.addEventListener("click",vaciarCarrito);
     actualizarTotales(totalCarrito,cantidadCarrito);
-    
-    
 }
 
-
+// =================    VACIAR CARRITO    ======================
+// -Vacia todo los elementos del carrito 
+// -guarda el carrito vacio en el localStorage
 function vaciarCarrito(){
-    let carrito= JSON.parse(localStorage.getItem("carrito")) || []; 
-    for(let i = 0;carrito.length;i++){
-        carrito.length=0;
+    if(carrito.length>0){
+        for(let i = 0;carrito.length;i++){
+            carrito.length=0;
+        }
+        actualizarCarrito(carrito);
+        window.location="index.html";
     }
-    localStorage.setItem("carrito",JSON.stringify(carrito));
-    window.location="index.html";
+    else{
+        alert("No hay productos en el carrito");
+    }
     
 }
+
+// =================    Actualizar Totales    ======================
+// -Actualizo la informacion de total y cantidad del carrito
 function actualizarTotales(importeTotal,cantidadTotal){
     const total= document.getElementById("total-carrito");
     const cantidad = document.getElementById("cantidad-carrito");
     total.textContent="Total = $"+importeTotal;
     cantidad.textContent="Cantidad = "+cantidadTotal;
 }
+
+// =================    Eliminar Producto    ======================
+// -Elimino el producto que recibo por parametro del carrito y 
+//  lo actualizo en el Storage
 function EliminarProducto(codigoProducto){
-    let carrito= JSON.parse(localStorage.getItem("carrito")) || []; 
     let actualizoCarrito=false;
-    for(let i = 0;carrito.length;i++){
-        const producto=carrito[i]
-        if(producto.id==codigoProducto){
-            carrito.splice(i,1);
-            actualizoCarrito=true;
-            break;
+        for(let i = 0;carrito.length;i++){
+            const producto=carrito[i]
+            if(producto.id==codigoProducto){
+                carrito.splice(i,1);
+                actualizoCarrito=true;
+                break;
+            }
         }
-    }
+    
     //Solo si encuentro el producto actualizo el carrito
     if(actualizoCarrito){
-        localStorage.setItem("carrito",JSON.stringify(carrito));
+        actualizarCarrito(carrito);
         mostrarCarrito();
     }
 }
 
+// =================    Restar Producto    ======================
+// -Resto la cantidad del producto que recibo por parametro en el  carrito y 
+//  lo actualizo en el Storage
 function restarProducto(codigoProducto){
-    //Consulto el carrito actual para buscar el producto
-    let carrito= JSON.parse(localStorage.getItem("carrito")) || []; 
     let actualizoCarrito=false;
     for(let i = 0;carrito.length;i++){
         const producto=carrito[i]
@@ -85,14 +103,15 @@ function restarProducto(codigoProducto){
     }
     //Solo si encuentro el producto actualizo el carrito
     if(actualizoCarrito){
-        localStorage.setItem("carrito",JSON.stringify(carrito));
+        actualizarCarrito(carrito);
         mostrarCarrito();
     }
 }
 
+// =================    Restar Producto    ======================
+// -Sumo la cantidad del producto que recibo por parametro en el  carrito y 
+//  lo actualizo en el localStorage
 function sumarProducto(codigoProducto){
-    //Consulto el carrito actual para buscar el producto
-    let carrito= JSON.parse(localStorage.getItem("carrito")) || []; 
     let actualizoCarrito=false;
     for(let i = 0;carrito.length;i++){
         const producto=carrito[i]
@@ -104,13 +123,36 @@ function sumarProducto(codigoProducto){
     }
     //Solo si encuentro el producto actualizo el carrito
     if(actualizoCarrito){
-        localStorage.setItem("carrito",JSON.stringify(carrito));
+        actualizarCarrito(carrito);
         mostrarCarrito();
     }
 
 }
+// =================    actualizar Carrito    ======================
+// -Actualizo el el carrito en el localStorage
+function actualizarCarrito(nuevoCarrito){
+     localStorage.setItem("carrito",JSON.stringify(nuevoCarrito));
+}
+
+
+// =================    MODO OSCURO    =================
+// Al hacer click, se cambia al modo oscuro, y cuando se vuelve a cliclear, se vuelve al modo claro
+// Se añade un addEventListener para registrar cuando el usuario haga click para cambiar entre los modos oscuro y claro
+const botonModoOscuro = document.getElementById("boton-modo-oscuro");
+botonModoOscuro.addEventListener("click", function () {
+    //console.log("Carrito actualizado:", carrito)
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        botonModoOscuro.textContent = 'Modo Oscuro';
+    } else {
+        botonModoOscuro.textContent = 'Modo Claro';
+    }
+});
+
+
 function init(){
     mostrarCarrito();
 }
+
 
 init();
--- js/main.js
+++ js/main.js
@@ -1,17 +1,39 @@
-// Esperar que el DOM cargue
-document.addEventListener("DOMContentLoaded", () => {
-  const btnContinuar = document.getElementById("btn-continuar");
+// =================    CARGAR CONFIGURACION BOTON CONTINUAR    =================
+// -Validamos que el campo nombre no este vacio
+// -Guardamos el nombre en el localstorage
+// -Redireccionamos a producto.html si hay un nombre
+function configurarBotonContinuar(){
+  const botonContinuar=document.getElementById("boton-continuar");
   const inputNombre = document.getElementById("nombre");
-
-  btnContinuar.addEventListener("click", () => {
+  botonContinuar.addEventListener("click", () => {
     const nombreCliente = inputNombre.value.trim();
 
     if (nombreCliente !== "") {
       localStorage.setItem("nombreCliente", nombreCliente);
-      window.location.href = "producto.html";
+      window.location.href = "productos.html";
     } else {
       alert("Por favor, ingresa tu nombre antes de continuar.");
     }
   });
+}
+
+// =================    MODO OSCURO    =================
+// Al hacer click, se cambia al modo oscuro, y cuando se vuelve a cliclear, se vuelve al modo claro
+// Se añade un addEventListener para registrar cuando el usuario haga click para cambiar entre los modos oscuro y claro
+const botonModoOscuro = document.getElementById("boton-modo-oscuro");
+botonModoOscuro.addEventListener("click", function () {
+    //console.log("Carrito actualizado:", carrito)
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        botonModoOscuro.textContent = 'Modo Oscuro';
+    } else {
+        botonModoOscuro.textContent = 'Modo Claro';
+    }
 });
 
+
+function init(){
+  configurarBotonContinuar();
+}
+
+init();
